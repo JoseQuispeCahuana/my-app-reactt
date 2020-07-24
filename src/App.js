2082630589c03284217f0838caf8dde8d0166f79
JoseQuispeCahuana <lacarpetadelvergazo1@gmail.com>
2020-07-24 09:59:19 -0500

    mi publicacion 1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Saludo from './components/saludo';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+        <Saludo/>
+      <h1>como esta</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/saludo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Saludo = (msj) => {
+    return(
+        <p>
+            <h1>EVALUACION 3</h1>
+        <h2> evaluacion de hola jose diego quispe cahuana</h2>
+        
+        
+        
+       </p>
+    )
+    }
+export default Saludo;
